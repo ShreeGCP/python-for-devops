b9262a7d070d3b80d2074a7e3c2700f38e34a945
ShreeGCP <139425446+ShreeGCP@users.noreply.github.com>
2024-07-06 12:04:46 +0530

    Update 01-string-len.py

--- Day-02/examples/01-string-len.py
+++ Day-02/examples/01-string-len.py
@@ -1,3 +1,8 @@
 text = "Python is awesome"
 length = len(text)
 print("Length of the string:", length)
+
+str1 = "Shridhara"
+str2 = "Devadiga"
+print(f"The length of {str1} is {len(str1)}")
+print(f"The length of {str2} is {len(str2)}")
